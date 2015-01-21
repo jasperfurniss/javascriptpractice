4cd9688c9be323e592d40fac9d13fa2da9fbf5bb
Jasper Furniss <jasperfurniss@gmail.com>
2015-01-21 09:13:42 -0500

    javascriptpractice

--- scripts/exercises-1.js
+++ scripts/exercises-1.js
@@ -34,7 +34,7 @@ function maxOfThree(a, b, c){
     }
 }
 
-console.assert(maxOfThree(1,2,3) == 3, "maxOfThree")
+console.assert(maxOfThree(1,2,3) == 3, "maxOfThree");
 
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
@@ -59,41 +59,23 @@ console.assert(isVowel('a'),'isVowel');
 //For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function translate(phrase){
-    var contentSplit = phrase.split("");
-    var result = '';
-
-    contentSplit.forEach(function(char){
-    if (isVowel(char) || char == " "){
-      result += char;
-    }else{
-      char = char + "o" + char;
-      result += char;
-    }
-});
-
-      return result;
-}
-
-console.log(translate('this is fun'));
-
-// function translate(phrase){
-//   // var vowels = ['a','e','i','o','u'];
-//   var splitWord = phrase.split('');
-//   var result = '';
-//
-//   splitWord.forEach(function (char){
-//     if (isVowel(char) || char == " ") {
-//       result += char;
-//     } else {
-//       char = char + 'o' + char;
-//       result += char;
+// function rovar(phrase){
+//     "use strict";
+//     var contentSplit = phrase.split("");
+//     var consanant = !isVowel;
+//     var result = "";
+//     contentSplit.forEach(function(letter){
+//     if (consanant(letter) || letter != " "){
+//       letter = letter + "o" + letter;
+//       result += letter;
+//     }else{
+//       result += letter;
 //     }
-//   });
-//   return result;
+//       return(result);
 // }
 //
-// console.log(translate('this is fun'));
+//     console.assert((rovar("this is fun") == "tothohisos isos fofunon"), 'rovar');
+
 
 // ---------------------
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers.
@@ -110,49 +92,76 @@ function sum(numbers){
     return sum;
 }
 
-console.assert(sum( [5, 10, 20, 100] ) == 135, 'sum');
+  console.assert(sum( [5, 10, 20, 100] ) == 135, 'sum');
 
 
-function multiply(){
+function multy(red){
     "use strict";
-    //...
+    var mult = 1;
+    red.forEach(function(bum){
+      mult *= bum;
+    });
+    return mult;
 }
+  console.assert(multy( [1, 2, 3, 4] ) == 24, 'multy');
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string.
 //For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
+function reverse(word){
     "use strict";
-    //...
+    return word.split('').reverse().join('');
 }
+
+  console.assert(reverse('jag testar')) == ('ratset gaj');
 
 // ---------------------
 // Write a function findLongestWord() that takes an array of words
 //and returns the length of the longest one.
 // ---------------------
 
-function findLongestWord(words){
+function findLongestWord(group){
     "use strict";
-    //...
-}
+      var str = group.split(" ");
+      var longest = 0;
+      var word = null;
+      str.forEach(function(str) {
+        if (longest < str.length) {
+          longest = str.length;
+          word = str;
+        }
+      });
+      return Number(word.length);
+    }
+console.assert(findLongestWord('a tale of two cities jabbermouth boy') == 11);
 
 // ---------------------
 // Write a function filterLongWords() that takes an array of words and
 //an integer i and returns the array of words that are longer than i.
 // ---------------------
 
-function filterLongWords(words, i){
-    "use strict";
-    //...
-}
+// function filterLongWords(words, i){
+//     "use strict";
+//     var longer = 0;
+//     var str = words.split(" ");
+//     str.forEach(function(str) {
+//       if (longer < str.length) {
+//       i > str.length;
+//       }return str;
+//     });
+// }
+//
+// console.assert(filterLongWords('hello, 4'));
+
 
 // ---------------------
-// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
+// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it.
+//Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 // ---------------------
 
 function charFreq(string){
     "use strict";
-    //...
+
 }
